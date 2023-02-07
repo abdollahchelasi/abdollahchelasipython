d70c585f4d79d2df594f288e1c0b2bb93f76ae4d
abdollahchelasi <abdollah.chelasi@gmail.com>
2023-02-07 23:05:18 +0330

    new

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ with st.container():
             st.image("images/pvc.png")
             st.write("""
 اجرای نصب پی وی سی در سراسر جزیره قشم        """)
-            st.markdown("[Pvc-Ramkan](https://pvcahmad.iran.liara.run/)")
+            st.markdown("[Pvc-Ramkan](https://pvcahmad.ir)")
         
    
     with col3:
@@ -76,7 +76,7 @@ with st.container():
                 st.image("images/upvc.png")
                 st.write("""
 تولیدی درب و پنجره UPVC نوین ترک , فروش و نصب PVC , طراحی یا ساخت و اجرای انواع سایبان PVC        """)
-                st.markdown("[Dekorasion Shademan](https://upvcahmad.iran.liara.run/)")
+                st.markdown("[Dekorasion Shademan](https://pvcshademan.ir)")
     
     
     
